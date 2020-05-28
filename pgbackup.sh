c4092b04d36b3db979af671693b6a5e519bc274a
Alexsandro Haag <alex@hgsoft.com.br>
2020-05-28 10:02:52 -0300

    Remove Log from panel

--- pgbackup.sh
+++ pgbackup.sh
@@ -2,9 +2,6 @@
 # pgbackup.sh - Backup do PostgreSQL
 # Criado em 2015-05-04 por Alexsandro Haag (alex@hgsoft.com.br)
 
-#Registrando log no Painel
-curl -u dev@hgsoft.com.br:159753hg -X POST -H "Content-Type: application/json" -d '{"project_name": "Backup Nuvem", "company_name": "HGSoft", "stage": "INICIO", "message": "Backup Iniciado - Postgres", "status": "OK", "date_time_sent": "'"$(date +%Y-%m-%dT%H:%M:%S)"'"}' --tlsv1 https://manager.hgsoft.com.br/logs
-
 BACKUP_DIR=/opt/hgsoft/backup
 TEMP_DIR=/tmp
 DMPFILE=nuvem.backup
@@ -22,7 +19,5 @@ tar $TAR_PAR $BACKUP_DIR/$BKPFILE.$BKPFORMAT $TEMP_DIR/$DMPFILE $TEMP_DIR/$LOGFI
 rm -rf $TEMP_DIR/$DMPFILE
 rm -rf $TEMP_DIR/$LOGFILE
 
-#Registrando log final no painel
-curl -u dev@hgsoft.com.br:159753hg -X POST -H "Content-Type: application/json" -d '{"project_name": "Backup Nuvem", "company_name": "HGSoft", "stage": "FINAL", "message": "Backup Finalizado - Postgres", "status": "OK", "date_time_sent": "'"$(date +%Y-%m-%dT%H:%M:%S)"'"}' --tlsv1 https://manager.hgsoft.com.br/logs
 date
 
